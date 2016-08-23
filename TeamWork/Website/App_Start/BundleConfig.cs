27efe2070d9a1bb9b97a1b8d559d980b40259e0f
elikrumova <eli.krumova.ek@gmail.com>
2016-08-23 22:10:35 +0300

    css

--- TeamWork/Website/App_Start/BundleConfig.cs
+++ TeamWork/Website/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace Website
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
         }
     }
 }
